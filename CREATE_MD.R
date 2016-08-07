ca91e4b8c8906da5e7470c311f15a49ac3049635
pablo14 <pcasas.biz@gmail.com>
2016-08-07 17:10:27 -0300

    structure and readme

--- CREATE_MD.R
+++ CREATE_MD.R
@@ -24,21 +24,21 @@ knit("miscellaneous/miscellaneous.Rmd", "miscellaneous/miscellaneous.md")
 ###         HTMLs             ###         
 #################################
 ## README
-knit2html("readme/readme.Rmd", "readme/README.md")
+knit2html("readme/readme.Rmd", "README.md");file.remove("README.md")
 
 ## SELECTING BEST VARIABLES
-knit2html("selecting_best_variables/introduction.Rmd", "selecting_best_variables/introduction.html")
-knit2html("selecting_best_variables/cross_plot.Rmd", "selecting_best_variables/cross_plot.html")
-knit2html("selecting_best_variables/plotar_histdens.Rmd", "selecting_best_variables/plotar_histdens.html")
-knit2html("selecting_best_variables/plotar_boxplot.Rmd", "selecting_best_variables/plotar_boxplot.html")
-knit2html("selecting_best_variables/correlation.Rmd", "selecting_best_variables/correlation.html")
+knit2html("selecting_best_variables/introduction.Rmd", "selecting_best_variables/introduction.html");file.remove("introduction.md")
+knit2html("selecting_best_variables/cross_plot.Rmd", "selecting_best_variables/cross_plot.html");file.remove("cross_plot.md")
+knit2html("selecting_best_variables/plotar_histdens.Rmd", "selecting_best_variables/plotar_histdens.html");file.remove("plotar_histdens.md")
+knit2html("selecting_best_variables/plotar_boxplot.Rmd", "selecting_best_variables/plotar_boxplot.html");file.remove("plotar_boxplot.md")
+knit2html("selecting_best_variables/correlation.Rmd", "selecting_best_variables/correlation.html");file.remove("correlation.md")
 
 ## DATA PREPARATION/PROFILING
-knit2html("data_preparation/outliers_treatment.Rmd", "data_preparation/outliers_treatment.html")
-knit2html("data_preparation/profiling.Rmd", "data_preparation/profiling.html")
+knit2html("data_preparation/outliers_treatment.Rmd", "data_preparation/outliers_treatment.html");file.remove("outliers_treatment.md")
+knit2html("data_preparation/profiling.Rmd", "data_preparation/profiling.html");file.remove("profiling.md")
 
 ## ASSESING MODEL PERFORMANCE 
-knit2html("model_performance/model_performance.Rmd", "model_performance/model_performance.html")
+knit2html("model_performance/model_performance.Rmd", "model_performance/model_performance.html");file.remove("model_performance.md")
 
 ## MISCELLANEOUS
-knit2html("miscellaneous/miscellaneous.Rmd", output = "miscellaneous/miscellaneous.html")
+knit2html("miscellaneous/miscellaneous.Rmd", output = "miscellaneous/miscellaneous.html");file.remove("miscellaneous.md")
