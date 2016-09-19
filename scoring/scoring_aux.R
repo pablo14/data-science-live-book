4924c04923cec95aa722c510e8d85fbb45610c50
pablo14 <pcasas.biz@gmail.com>
2016-09-18 23:03:18 -0300

    scoring

--- scoring/scoring_aux.R
+++ scoring/scoring_aux.R
@@ -1,6 +1,6 @@
 library(rpivotTable)
 
-data=read.delim(file="example.txt", sep="\t", header = T, stringsAsFactors=F)
+data=read.delim(file="scoring/example.txt", sep="\t", header = T, stringsAsFactors=F)
 
 
 ## score_cut_point, all obove "0.5" will be classified as "yes"
@@ -22,7 +22,5 @@ data$label_predicted=ifelse(data$score>=0.4, "yes", "no")
 ## Normal confusion matrix
 rpivotTable(data = data, rows = "label_predicted", cols="target", aggregatorName = "Count", rendererName = "Table", width="100%", height="400px")
 
-
-## This view sums 100% per column
 rpivotTable(data = data, rows = "label_predicted", cols="target", aggregatorName = "Count as Fraction of Columns", rendererName = "Table", width="100%", height="400px")
   
