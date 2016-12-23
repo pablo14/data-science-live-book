cee813efa35da5a4168c728a606c61c32fa73069
pablo14 <pcasas.biz@gmail.com>
2016-12-23 19:36:05 -0300

    high cardinality in predictive model

--- data_preparation/test.R
+++ data_preparation/test.R
@@ -1,13 +1,13 @@
 library(caret)
-fitControl <- trainControl(method = "cv", 
-                           number = 4, 
+fitControl <- trainControl(method = "cv",
+                           number = 4,
                            classProbs = TRUE,
                            summaryFunction = twoClassSummary)
 
 
-fit_gbm_1 <- train(has_flu ~ country, 
-                 data = data_country, 
-                 method = "gbm", 
+fit_gbm_1 <- train(has_flu ~ country,
+                 data = data_country,
+                 method = "gbm",
                  trControl = fitControl,
                  verbose = FALSE,
                  metric = "ROC")
@@ -21,16 +21,18 @@ gain_lift(data_country,str_score = 'score', str_target = 'has_flu')
 
 my_roc=roc(response=data_country$has_flu, predictor=data_country$score);my_roc
 
+
 ##############
 data_country$has_flu2=ifelse(data_country$has_flu=="no", 0, 1)
-grp2=dplyr::group_by(data_country, country) %>% dplyr::summarise(sum_paid=sum(has_flu2), 
-                                                   perc_paid=sum(has_flu2)/sum(data_country$has_flu2), 
-                                                   mean_paid=round(mean(has_flu2),3), 
-                                                   q_rows=n(), 
+grp2=dplyr::group_by(data_country, country) %>% dplyr::summarise(sum_paid=sum(has_flu2),
+                                                   perc_paid=sum(has_flu2)/sum(data_country$has_flu2),
+                                                   mean_paid=round(mean(has_flu2),3),
+                                                   q_rows=n(),
                                                    p_rows=round(n()/nrow(data_country),3)) %>%
                                                           arrange(-sum_paid)
 grp2
-
+a=categ_analysis(data = data_country, str_input = 'country', str_target = "has_flu")
+a
 #####################
 d=select(grp2, -country, -p_rows)
 set.seed(11112)
@@ -38,7 +40,7 @@ fit=kmeans(scale(data.frame(d)),8)
 fit$centers;fit$size
 
 ## Call the function
-grp$country_group=paste("group_", fit$cluster, sep = "") 
+grp$country_group=paste("group_", fit$cluster, sep = "")
 
 data_country=merge(data_country, select(grp, country, country_group))
 
