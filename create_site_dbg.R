1b986fa0e1612b3defe411921580f64d8ff61bcc
pablo14 <pcasas.biz@gmail.com>
2017-11-11 12:50:49 -0300

    - typos (thanks Jonas Ertel!) - delete unused files

--- create_site_dbg.R
+++ create_site_dbg.R
@@ -1,4 +1,4 @@
-create_site <- function(is_pdf=F, high)
+create_site <- function(is_pdf=F)
 {
   fn="data-science-live-book.Rmd"
   if (file.exists(fn)) file.remove(fn)
@@ -7,14 +7,17 @@ create_site <- function(is_pdf=F, high)
   {
     rmarkdown::render_site(output_format = 'bookdown::gitbook', encoding = 'UTF-8')
   } else {
-    rmarkdown::render_site(output_format = bookdown::pdf_book(template='template.tex', latex_engine = "pdflatex"))
+    rmarkdown::render_site(output_format = bookdown::pdf_book(template='template.tex', latex_engine = "pdflatex",
+                                                              split_bib = T,toc_unnumbered=F))
   }
 }
 
 
-create_site(T)
 create_site(F)
+#create_site(F)
 
 
 
+
+rmarkdown::render_site(output_format =  bookdown::epub_book(toc=T,toc_depth = 2), encoding = 'UTF-8')
 
