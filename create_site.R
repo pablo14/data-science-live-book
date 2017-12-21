971bea291669d1131644c55cd6309f932e0ef5e6
pablo14 <pcasas.biz@gmail.com>
2017-12-21 19:01:31 -0300

    disable pdf generation on site

--- create_site.R
+++ create_site.R
@@ -1,5 +1,5 @@
 rmarkdown::render_site(output_format = 'bookdown::gitbook', encoding = 'UTF-8')
-rmarkdown::render_site(output_format = bookdown::pdf_book(template='template.tex', latex_engine = "pdflatex"))
+# rmarkdown::render_site(output_format = bookdown::pdf_book(template='template.tex', latex_engine = "pdflatex"))
 
 
 
